78ccd90fb7af5c56b36c76212a6f1eb52d9aff55
dejima-yuya <jimajapan33@yahoo.co.jp>
2023-06-30 16:45:20 +0900

    コンフリクト解消

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 2023_06_30_065255) do
   create_table "recipes", force: :cascade do |t|
     t.string "title"
     t.text "content"
+  end
+  
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
